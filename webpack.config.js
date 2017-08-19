d978b6c8bfdaaaa6d6454ea15e488bdf5b3ad784
helijun <twobike@163.com>
2017-08-19 15:41:53 +0800

    hello word

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    entry: './main.js', // 入口文件路径
+    output: {
+        path: '/',
+        filename: 'index.js'
+    },
+    devServer: {
+        inline: true,
+        port: 3333
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/, // babel 转换为兼容性的 js
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'latest']
+                }
+            }
+        ]
+    }
+}
